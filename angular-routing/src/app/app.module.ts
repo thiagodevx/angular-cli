f4a53cdfef5b313ac683603160ac95cfc1e617b8
thiago.dev.x <thiago.dev.x@gmail.com>
2020-02-22 19:41:34 -0300

    adding some components from angular material!

--- angular-routing/src/app/app.module.ts
+++ angular-routing/src/app/app.module.ts
@@ -14,13 +14,21 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     CustomerComponent,
-    NavComponent
+    NavComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +40,13 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
