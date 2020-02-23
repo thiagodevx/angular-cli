d1c49a8bcc9c898efda4c1691b7d3509dec302b3
thiago.dev.x <thiago.dev.x@gmail.com>
2020-02-23 16:29:15 -0300

    adding some code to check coverage

--- angular-routing/src/app/admin/users/users.component.ts
+++ angular-routing/src/app/admin/users/users.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  users: any[];
+  hasPermission = false;
   constructor() { }
 
   ngOnInit() {
+    if (this.hasPermission) {
+      this.getUsers().then(users => this.users = users)
+        .catch(error => console.log(error));
+    }
+  }
+  async getUsers(): Promise<any> {
+    return [
+      {name: 'john', email: 'john@angular.com'},
+      {name: 'colleen', email: 'colleen@angular.com'}
+    ];
   }
 
 }
